2f150661d93bc85ba7c14f9eba3756fd9c621da9
Usuario <usuario@ejemplo.com>
2026-05-16 18:21:00 -0500

    Quitar no-cors para ver errores

--- js/admin.js
+++ js/admin.js
@@ -183,8 +183,7 @@ window.handlePublishDeal = async function(e) {
   const scriptUrl = `https://script.google.com/macros/s/AKfycbxNPK7kxCPUqamIzD9X5DUuE6wmoKT_zuRH3H8gXEk4_ZJfZ7kg6Z7xeoeOR_osUNxfqw/exec?notifyEmail=${document.getElementById('notify-email').checked}&title=${encodeURIComponent(title)}&price=${encodeURIComponent(formatCurrency(dealPrice))}&store=${encodeURIComponent(store)}&url=${encodeURIComponent(url)}`;
   
   fetch(scriptUrl, {
-    method: 'GET',
-    mode: 'no-cors'
+    method: 'GET'
   })
   .then(() => console.log('Notificación enviada con éxito'))
   .catch(err => console.error('Error al enviar notificación:', err));

347a4234cd35f6c40e6b9bb0d4c8e93e2e23c89b
Usuario <usuario@ejemplo.com>
2026-05-16 15:10:14 -0500

    Actualizar URL de notificaciones con proteccion

--- js/admin.js
+++ js/admin.js
@@ -192,7 +192,7 @@ window.handlePublishDeal = async function(e) {
   };
 
   console.log('Enviando notificación a Google Sheets...');
-  fetch('https://script.google.com/macros/s/AKfycbxrwHecfCwiLG6WLS6f53x69Wg1_rYjEGR5G1XSYN8XxQ8wF0yy_FEan7XH0RQOJQJdvQ/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbx53Ry4fUMOs7SciRAHZVnKTT790JiESSNVpXjBU64HGiadw08xjK8KrHt0vs-wHuMuIA/exec', {
     method: 'POST',
     mode: 'no-cors',
     body: JSON.stringify(notifyData)

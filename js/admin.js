b3d701c7ddd76b75c3ffe8dc42e0dc2947e80f42
Usuario <usuario@ejemplo.com>
2026-05-16 14:22:34 -0500

    Actualizar URL de notificaciones v2

--- js/admin.js
+++ js/admin.js
@@ -194,7 +194,7 @@ window.handlePublishDeal = async function(e) {
   formData.append('notifyPhone', notifyPhone);
 
   console.log('Enviando notificación a Google Sheets...');
-  fetch('https://script.google.com/macros/s/AKfycbxk0pj3eDLfnj95x2Z1dBrmI1kziF_gmPEE4SedgIplRUCEtgpEhDuydB-z1MiVD09snw/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbzV41PMQpEnrpuZMsVNxh_FI11Dc7rlyyCRfFjo413f9I4kfscNP1gRfEv3uUkqMBJctA/exec', {
     method: 'POST',
     mode: 'no-cors',
     body: formData

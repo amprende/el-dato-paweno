df3c956b4d3a15df6a1641dc33247b51af3c728c
Usuario <usuario@ejemplo.com>
2026-05-16 18:34:17 -0500

    Agregar alert de depuración

--- js/admin.js
+++ js/admin.js
@@ -180,6 +180,7 @@ window.handlePublishDeal = async function(e) {
   console.log('Enviando notificación a Google Sheets (GET)...');
   const scriptUrl = `https://script.google.com/macros/s/AKfycbxNPK7kxCPUqamIzD9X5DUuE6wmoKT_zuRH3H8gXEk4_ZJfZ7kg6Z7xeoeOR_osUNxfqw/exec?notifyEmail=${document.getElementById('notify-email').checked}&title=${encodeURIComponent(title)}&price=${encodeURIComponent(formatCurrency(dealPrice))}&store=${encodeURIComponent(store)}&url=${encodeURIComponent(url)}`;
   
+  alert('DEBUG - URL llamada: ' + scriptUrl);
   console.log('URL llamada:', scriptUrl);
 
   fetch(scriptUrl, {

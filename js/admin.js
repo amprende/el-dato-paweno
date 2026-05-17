f0b1dc961162ef9d16039e3dd1220b977efd77fc
Usuario <usuario@ejemplo.com>
2026-05-17 10:18:19 -0500

    Update Google Script URL

--- js/admin.js
+++ js/admin.js
@@ -184,7 +184,7 @@ window.handlePublishDeal = async function(e) {
 
   if (notifyEmail || notifyPhone) {
     console.log('Enviando señal de notificación a Google Sheets...');
-    const scriptUrl = `https://script.google.com/macros/s/AKfycbwmx__O4EHOAXQ4kS_SSj_GZPF3wWMZpqiiM7bKCR8kHUafyXBFtU2N346k-VGn6xXG1A/exec`;
+    const scriptUrl = `https://script.google.com/macros/s/AKfycbxCiwi7pqmXEVwCQ50Y2A8L-RkjXps_zOpNMzFyEpsCYeJMCBtXw0OlQPibXOPp-gWX5Q/exec`;
     const payload = {
       notifyEmail: notifyEmail,
       notifyPhone: notifyPhone,
